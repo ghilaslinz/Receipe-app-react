ab78363e2841787f85c970b9dc05691b357f3e30
ghilaslinz <44534106+ghilaslinz@users.noreply.github.com>
2023-04-12 14:30:51 +0200

    Update Receipe.js

--- src/Receipe.js
+++ src/Receipe.js
@@ -17,9 +17,40 @@ function Receipe({recipe}) {
     }
     return (
      <>
-  
+             <Card sx={{ maxWidth: 345, margin : '20px' }}>
+      <CardMedia
+        component="img"
+        height="300"
+        src={image} 
+        alt={label}
+      
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {label}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        <url href={url} />
+        </Typography>
+      </CardContent>
+      <CardActions className="ingredients__show">
+        
+        <div   className="button__ingredients"><Button onClick={showIngredientsfunction}  variant="contained">Show Ingredients</Button></div>
+        <div>
+        {showIngredients ? (
+          <div>
+        <Ingredients ingredients={ingredients} />
+        <Link rel="noopener noreferrer" href={url}  target="_blank" >See More</Link>
+        </div>
+        ) : <div></div>
+      }
+      </div>
+      
+      </CardActions>
+    </Card>
           
             <url href={url} />
+
         
         </>
     )
